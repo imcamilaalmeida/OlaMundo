5cdfbf92ae5f8f4fb1adddfb1cfa68c7d6edabcd
Camila Almeida <camilaalma2008@gmail.com>
2023-07-31 11:11:57 -0300

    Projeto criado e mensagem para o usuário pronta

---
+++ variavel.py
@@ -0,0 +1,11 @@
+# input é uma função que permite perguntar algo ao usuário
+nome = input("Qual é o seu nome?")
+
+'''
+Bloco 
+de 
+Comentário
+Permite que varias linhas se tornem um comentário
+'''
+
+print("Olá,", nome, ". Tudo bem com você?")
