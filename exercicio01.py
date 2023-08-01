ce61366234093564124d2de6ae03c5c62248bf19
Camila Almeida <camilaalma2008@gmail.com>
2023-08-01 10:00:46 -0300

    alterações de primeiro de agosto

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+a = float(input("Informe sua nota do primeiro trimestre: "))
+b = float(input("Informe sua nota do segundo trimestre: "))
+c = float(input("Informe sua nota do terceiro trimestre: "))
+print("a: ", a, "b: ", b, "c: ", c)
+
+media = (a + b + c) / 3
+
+print("Sua média é", media)
---
+++ exercicio02.py
@@ -0,0 +1,3 @@
+num1 = int(input("Informe um número: "))
+
+print("O antecessor é", num1 - 1, "e o sucessor é", num1 + 1)
